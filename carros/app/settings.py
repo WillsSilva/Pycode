eca34c43a945804d11dbfd223afe584bfc239ade
Willian Andreola da Silva <wills.andreola@gmail.com>
2024-11-12 20:21:02 -0300

    banco
    banco

--- carros/app/settings.py
+++ carros/app/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'carros',
         'USER': 'root',
-        'PASSWORD': '12345678',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
